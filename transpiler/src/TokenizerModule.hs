f3142202e17b1d51c941e7262c2b8f688814e8e1
david <david@debian.home>
2023-07-26 01:36:57 +0200

    now it generates it in correct format and handles transparency correctly

--- transpiler/src/TokenizerModule.hs
+++ transpiler/src/TokenizerModule.hs
@@ -7,10 +7,12 @@ data Div = Div
     , br :: Int  ---------------
     , bg :: Int  -- BG colors -- 
     , bb :: Int  ---------------
+    , ba :: Float
 
     , fr :: Int  ---------------
     , fg :: Int  -- FG colors --  
     , fb :: Int  ---------------
+    , fa :: Float
 
     , margin :: Int
     , marginUnit :: String
@@ -20,7 +22,7 @@ data Div = Div
 
 
 toTag :: Div -> String
-toTag div = "<div style=\"background-color:rgb(" ++ show (br div) ++ ", " ++ show (bg div) ++ ", " ++ show (bb div) ++ "), color:rgb(" ++ show (fr div) ++ ", " ++ show (fg div) ++ ", " ++ show (fb div) ++ "), margin:" ++ show (margin div) ++ marginUnit div ++ ", padding:" ++ show (padding div) ++ paddingUnit div ++ "\">" ++ content div ++ "</div>\n"
+toTag div = "<div style=\"background-color: rgba(" ++ show (br div) ++ ", " ++ show (bg div) ++ ", " ++ show (bb div) ++ ", " ++ show (ba div) ++ "); color: rgba(" ++ show (fr div) ++ ", " ++ show (fg div) ++ ", " ++ show (fb div) ++ " ," ++ show (fa div) ++ "); margin: " ++ show (margin div) ++ marginUnit div ++ "; padding: " ++ show (padding div) ++ paddingUnit div ++ ";\">" ++ content div ++ "</div>\n"
 
 errorDiv :: Div
 errorDiv = Div 
@@ -30,16 +32,41 @@ errorDiv = Div
     , br = 255  ---------------
     , bg =   0  -- BG colors -- 
     , bb = 255  ---------------
+    , ba = 1.0
 
     , fr =   0  ---------------
     , fg =   0  -- FG colors --  
     , fb =   0  ---------------
+    , fa =   1.0
 
     , margin = 10
     , marginUnit = "px"
     , padding = 10
     , paddingUnit = "px"
     }
+
+
+defaultDiv :: Div
+defaultDiv = Div 
+    { content = ""
+    , font = "13px Arial, sans-serif"
+
+    , br = 255  ---------------
+    , bg =   0  -- BG colors -- 
+    , bb = 255  ---------------
+    , ba =   0
+
+    , fr =   0  ---------------
+    , fg =   0  -- FG colors --  
+    , fb =   0  ---------------
+    , fa =   1.0
+
+    , margin = 10
+    , marginUnit = "px"
+    , padding = 10
+    , paddingUnit = "px"
+    }
+
 
 
 tokenize :: String -> [Div]
